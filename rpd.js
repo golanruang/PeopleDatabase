44364ba2856e5d55577728337f4c2b272aa92a04
Bickolas Liu <51180693+liunicholas@users.noreply.github.com>
2020-01-27 11:39:35 -0800

    Update rpd.js

--- rpd.js
+++ rpd.js
@@ -1,248 +1,214 @@
 //data for all rich people
-var richPeopleData = {"people": [{
-"id": 69420,
-"name": "Bill Gates",
-"netWorth": "1.1e11",
-"occupation": "rich",
-"chonkiness": "5",
-"companies": [12345],
-"alphaLevel": "2",
-"attractiveness": "10",
-},
-
-{
-"id": 89564,
-"name": "Jeff Bezos",
-"netWorth": "1.1e11",
-"occupation": "CEO of Amazon",
-"chonkiness": "10",
-"companies": [23456, 88888],
-"alphaLevel": "8",
-"attractiveness": "6",
-},
-
-{
-"id": 50568,
-"name": "Warren Buffett",
-"netWorth": "8.7e10",
-"occupation": "makes money",
-"chonkiness": "9",
-"companies": [89012],
-"alphaLevel": "7",
-"attractiveness": "6",
-},
-
-{
-"id": 07984,
-"name": "Bernard Arnault",
-"netWorth": "1.0e11",
-"occupation": "CEO boi",
-"chonkiness": "5",
-"companies": [90123],
-"alphaLevel": "3",
-"attractiveness": "3",
-},
-
-{
-"id": 94406,
-"name": "Mark Zuckerberg",
-"netWorth": "7.4e10",
-"occupation": "not being a robot",
-"chonkiness": "6",
-"companies": [67890],
-"alphaLevel": "10",
-"attractiveness": "3",
-},
-
-{
-"id": 16131,
-"name": "Amancio Ortega",
-"netWorth": "7.7e10",
-"occupation": "fashion dude",
-"chonkiness": "3",
-"companies": [01234],
-"alphaLevel": "3",
-"attractiveness": "0",
-},
-
-{
-"id": 35029,
-"name": "Larry Ellison",
-"netWorth": "6.8e10",
-"occupation": "having a fine mustache",
-"chonkiness": "3",
-"companies": [78901],
-"alphaLevel": "7",
-"attractiveness": "7",
-},
-
-{
-"id": 56544,
-"name":"Donald Trump",
-"netWorth": "3.1e9",
-"occupation": "professional tweeter",
-"chonkiness": "9001",
-"companies": [15973],
-"alphaLevel": "69",
-"attractiveness": "-1,000000000",
-},
-
-{
-"id": 86243,
-"name": "Larry Page",
-"netWorth": "5.7e10",
-"occupation": "unemployed",
-"chonkiness": "5",
-"companies": [56789],
-"alphaLevel": "2",
-"attractiveness": "2",
-},
-
-{
-"id": 09138,
-"name": "Carlos Slim",
-"netWorth": "7.6e10",
-"occupation": "being slim",
-"chonkiness": "1",
-"companies": [13579],
-"alphaLevel": "2",
-"attractiveness": "1",
-},
-
-{
-"id": 98254,
-"name": "Mukesh Ambani",
-"netWorth": "6.1e10",
-"occupation": "business man",
-"chonkiness": 10,
-"companies": [24680],
-"alphaLevel": 8,
-"attractiveness": 7,
-},
-
-{
-"id": 45233,
-"name": "Sergey Brin",
-"netWorth": "5.3e10",
-"occupation": "google",
-"chonkiness": "10",
-"companies": [56789],
-"alphaLevel": 8,
-"attractiveness": 10,
-}],
-
-"companies": [
-{
-"id": 12345,
-"name": "microsoft",
-"stonks": "33243224432",
-"employees": "1.5e5",
-"description": "making computers and windows",
-},
-
-{
-"id": 23456,
-"name": "amazon",
-"stonks": "6.9e12",
-"employees": "7.5e5",
-"description": "not paying taxes",
-},
-{
-"id": 34567,
-"name": "apple",
-"stonks": "3.8e13",
-"employees": "1.3e5",
-"description": "iPhone 2049S SE+ with thirteen additional cameras",
-},
-
-{
-"id": 45678,
-"name": "tesla",
-"stonks": "3.9e10",
-"employees": "4.5e4",
-"description": "electric bots",
-},
-
-{
-"id": 56789,
-"name": "google",
-"stonks": "4.8e12",
-"employees": "1.0e5",
-"description": "big brother",
-},
-
-{
-"id": 67890,
-"name": "facebook",
-"stonks": "2.6e10",
-"employees": "4.0e4",
-"description": "like a book with faces",
-},
-
-{
-"id": 78901,
-"name": "oracle",
-"stonks": "1.4e11",
-"employees": "1.4e6",
-"description": "2 million devices",
-},
-
-{
-"id": 15973,
-"name": "Trump Sales and Leasing",
-"stonks": "4.2e11",
-"employees": 3000,
-"description": "sells things with Trump’s name on it",
-},
-
-{
-"id": 89012,
-"name": "berkshire hathaway",
-"stonks": "4.2e11",
-"employees": 389000,
-"description": "stonks",
-},
-
-{
-"id": 90123,
-"name": "LVMH moet hennessy",
-"stonks": "4.2e11",
-"employees": 136000,
-"description": "rich french bois",
-},
-
-{
-"id": 01234,
-"name": "zara",
-"stonks": "4.2e11",
-"employees": 174000,
-"description": "dress me up like one of your french girls",
-},
-
-{
-"id": 13579,
-"name": "america movil",
-"stonks": "4.2e11",
-"employees": 189000,
-"description": "el telecom de meheeko",
-},
-
-{
-"id": 24680,
-"name": "reliance limited",
-"stonks": "4.2e11",
-"employees": 29000,
-"description": "rich indian bois that aren't in comp sci",
-},
-
-{
-"id": 88888,
-"name": "blue origin",
-"stonks": "4.2e11",
-"employees": 2600,
-"description": "spaaaaccccceeee",
-}
-]};
+var richPeopleData = {
+	"people":[
+		{
+			"id":69420,
+			"name":"Bill Gates",
+			"netWorth":"1.1e11",
+			"occupation":"rich",
+			"chonkiness":"5",
+			"companies":[12345],
+			"alphaLevel":"2",
+			"attractiveness":"10",
+		},
+		{
+			"id":89564,
+			"name":"Jeff Bezos",
+			"netWorth":"1.1e11",
+			"occupation":"CEO of Amazon",
+			"chonkiness":"10",
+			"companies":[23456,88888],
+			"alphaLevel":"8",
+			"attractiveness":"6",
+		},
+		{
+			"id":50568,
+			"name":"Warren Buffett",
+			"netWorth":"8.7e10",
+			"occupation":"makes money",
+			"chonkiness":"9",
+			"companies":[89012],
+			"alphaLevel":"7",
+			"attractiveness":"6",
+		},
+		{
+			"id":07984,
+			"name":"Bernard Arnault",
+			"netWorth":"1.0e11",
+			"occupation":"CEO boi",
+			"chonkiness":"5",
+			"companies":[90123],
+			"alphaLevel":"3",
+			"attractiveness":"3",
+		},
+		{
+			"id":94406,
+			"name":"Mark Zuckerberg",
+			"netWorth":"7.4e10",
+			"occupation":"not being a robot",
+			"chonkiness":"6",
+			"companies":[67890],
+			"alphaLevel":"10",
+			"attractiveness":"3",
+		},
+		{
+			"id":16131,
+			"name":"Amancio Ortega",
+			"netWorth":"7.7e10",
+			"occupation":"fashion dude",
+			"chonkiness":"3",
+			"companies":[01234],
+			"alphaLevel":"3",
+			"attractiveness":"0",
+		},
+		{
+			"id":35029,
+			"name":"Larry Ellison",
+			"netWorth":"6.9e10",
+			"occupation":"having a fine mustache",
+			"chonkiness":"3",
+			"companies":[78901],
+			"alphaLevel":"7",
+			"attractiveness":"7",
+		},
+		{
+			"id":56544,
+			"name":"Donald Trump",
+			"netWorth":"3.1e9",
+			"occupation":"professional tweeter",
+			"chonkiness":"9001",
+			"companies":[15973],
+			"alphaLevel":"69",
+			"attractiveness":"-1000000000",
+		},
+		{
+			"id":86243,
+			"name":"Larry Page",
+			"netWorth":"5.7e10",
+			"occupation":"unemployed",
+			"chonkiness":"5",
+			"companies":[56789],
+			"alphaLevel":"2",
+			"attractiveness":"2",
+		},
+		{
+			"id":09138,
+			"name":"Carlos Slim",
+			"netWorth":"7.6e10",
+			"occupation":"being slim",
+			"chonkiness":"1",
+			"companies":[13579],
+			"alphaLevel":"2",
+			"attractiveness":"1",
+		},
+		{
+			"id":98254,
+			"name":"Mukesh Ambani",
+			"netWorth":"6.1e10",
+			"occupation":"business man",
+			"chonkiness":"10",
+			"companies":[24680],
+			"alphaLevel":"8",
+			"attractiveness":"7",
+		},
+		{
+			"id":45233,
+			"name":"Sergey Brin",
+			"netWorth":"5.3e10",
+			"occupation":"google",
+			"chonkiness":"10",
+			"companies":[56789],
+			"alphaLevel":"8",
+			"attractiveness":"10",
+		},
+	],
+	"companies":[
+		{"id":12345,
+			"name":"microsoft",
+			"stonks":"33243224432",
+			"employees":"1.5e5",
+			"description":"making computers and windows",
+		},
+		{"id":23456,
+			"name":"amazon",
+			"stonks":"6.9e12",
+			"employees":"7.5e5",
+			"description":"not paying taxes",
+		},
+		{"id":34567,
+			"name":"apple",
+			"stonks":"3.8e13",
+			"employees":"1.3e5",
+			"description":"iPhone2049S SE+ with thirteen additional cameras",
+		},
+		{"id":45678,
+			"name":"tesla",
+			"stonks":"3.9e10",
+			"employees":"4.5e4",
+			"description":"electric bots",
+		},
+		{"id":56789,
+			"name":"google",
+			"stonks":"4.8e12",
+			"employees":"1.0e5",
+			"description":"big brother",
+		},
+		{"id":67890,
+			"name":"facebook",
+			"stonks":"2.6e10",
+			"employees":"4.0e4",
+			"description":"like a book with faces",
+		},
+		{"id":78901,
+			"name":"oracle",
+			"stonks":"1.4e11",
+			"employees":"1.4e6",
+			"description":"2 million devices",
+		},
+		{"id":15973,
+			"name":"Trump Sales and Leasing",
+			"stonks":"4.2e11",
+			"employees":"3000",
+			"description":"sells things with Trump’s name on it",
+		},
+		{"id":89012,
+			"name":"berkshire hathaway",
+			"stonks":"4.2e11",
+			"employees":"389000",
+			"description":"stonks",
+		},
+		{"id":90123,
+			"name":"LVMH moet hennessy",
+			"stonks":"4.2e11",
+			"employees":"136000",
+			"description":"rich french bois",
+		},
+		{"id":01234,
+			"name":"zara",
+			"stonks":"4.2e11",
+			"employees":"174000",
+			"description":"dress me up like one of your french girls",
+		},
+		{"id":13579,
+			"name":"america movil",
+			"stonks":"4.2e11",
+			"employees":"189000",
+			"description":"el telecom de meheeko",
+		},
+		{"id":24680,
+			"name":"reliance limited",
+			"stonks":"4.2e11",
+			"employees":"29000",
+			"description":"rich indian bois that aren’t in comp sci",
+		},
+		{"id":88888,
+			"name":"blue origin",
+			"stonks":"4.2e11",
+			"employees":"2600",
+			"description":"spaaaaccccceeee",
+		},
+	]
+};
 
 function convExp(string) {
   var beginInt = string.substring(0,3);
@@ -406,6 +372,19 @@ function sortRows() {
     }
   }
   richPeopleData["people"] = sortedArray;
+  updateTable();
+}
+
+function addPerson() {
+  var id = Math.floor(Math.random()*90000) + 10000;
+  var nm = document.getElementById("nmInput").value;
+  var nw = document.getElementById("nwInput").value;
+  var occ = document.getElementById("occInput").value;
+  var chk = document.getElementById("chkInput").value;
+  var comp = document.getElementById("compInput").value;
+  var al = document.getElementById("alInput").value;
+  var att = document.getElementById("attInput").value;
+  richPeopleData["people"].push({"id":id,"name":nm,"netWorth":nw,"occupation":occ,"chonkiness":chk,"companies":[comp],"alphaLevel":al,"attractiveness":att,},)
   updateTable();
 }
 
